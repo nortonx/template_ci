aa97b185d22d60dce97fb6770add7604936a68df
Norton Almeida <norton.almeida@gmail.com>
2011-03-27 21:48:50 -0300

    Another cleanup

---
+++ application/views/admin/form.php
@@ -0,0 +1,10 @@
+<div id="content" class="grid_16">
+	<div id="form_login" class="grid_6">
+		<?php
+		echo form_open('action');
+		
+		
+		echo form_close();
+		?>
+	</div>
+</div>
---
+++ assets/js/config.js
@@ -0,0 +1,30 @@
+﻿/*
+Copyright (c) 2003-2011, CKSource - Frederico Knabben. All rights reserved.
+For licensing, see LICENSE.html or http://ckeditor.com/license
+*/
+
+CKEDITOR.editorConfig = function( config )
+{
+	// Define changes to default configuration here. For example:
+	 config.language = 'pt-br';
+	 config.skin = 'v2';
+	 config.toolbar_Full =
+	 [
+	    ['Source','-',],
+	    ['Cut','Copy','Paste','PasteText','PasteFromWord'],
+	    ['Undo','Redo','-','Find','Replace','-','SelectAll','RemoveFormat'],
+	    '/',
+	    ['Bold','Italic','Underline','Strike'],
+	    ['NumberedList','BulletedList','-','Outdent','Indent','Blockquote','CreateDiv'],
+	    ['JustifyLeft','JustifyCenter','JustifyRight','JustifyBlock'],
+	    ['BidiLtr', 'BidiRtl' ],
+	    ['Link','Unlink','Anchor'],
+	    ['Image','Flash','Table','HorizontalRule','Smiley','SpecialChar','PageBreak','Iframe'],
+	    '/',
+	    ['Styles','Format','Font','FontSize'],
+	    ['TextColor','BGColor'],
+	    ['Maximize', 'ShowBlocks','-','About']
+	 ];
+	 
+	// config.uiColor = '#AADC6E';
+};
