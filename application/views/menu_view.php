4a98934ba24a01d5281e91455ba3bbc67ae2c0f4
Norton Almeida <norton.almeida@gmail.com>
2011-07-28 02:22:15 -0300

    Fixing the menu view

--- application/views/menu_view.php
+++ application/views/menu_view.php
@@ -2,12 +2,12 @@
     <ul>
         <li>
             <?php echo anchor('controller/action', 'Option1', 'title="teste1"'); ?>
-            <?php echo anchor('controller/action', 'Option2', 'title="teste1"'); ?>
-            <?php echo anchor('controller/action', 'Option3', 'title="teste1"'); ?>
-            <?php echo anchor('controller/action', 'Option4', 'title="teste1"'); ?>
-            <?php echo anchor('controller/action', 'Option5', 'title="teste1"'); ?>
-            <?php echo anchor('controller/action', 'Option6', 'title="teste1"'); ?>
-            <?php echo anchor('controller/action', 'Option7', 'title="teste1"'); ?>
+		</li>
+		<li>
+			<?php echo anchor('controller/action', 'Option2', 'title="teste1"'); ?>
+		</li>
+        <li>    
+        	<?php echo anchor('controller/action', 'Option2', 'title="teste1"'); ?>
         </li>
     </ul>
 </div>
